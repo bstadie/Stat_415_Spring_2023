99b07ee5e19f514ddc44a183182ab20a1d7f7576
Bradly Stadie <bstadie@vectorinstitute.ai>
2023-04-29 02:56:03 -0500

    Update make-us-map.py

--- homework-3/make-us-map.py
+++ homework-3/make-us-map.py
@@ -1,3 +1,5 @@
 import plotly.express as px 
 fig = px.choropleth(KMEAN_df, locations='STATE_ABBR', locationmode='USA-states', color='DRUNK_DRIVING_PERCENTAGE_CLUSTER_MEAN', scope='usa') 
 fig.show()
+
+# alternatively, use plotly -- https://datascience.stackexchange.com/questions/9616/how-to-create-us-state-choropleth-map
